d75a50312e7cb599bc9123655783643be5c6f87d
2676959 <p2676959@gmail.com>
2024-07-18 18:05:25 +0800

    added for easier nbt creation

---
+++ src/main/java/uce/utils/nbtHelper.java
@@ -0,0 +1,24 @@
+package uce.utils;
+
+import net.minecraft.nbt.NbtFloat;
+import net.minecraft.nbt.NbtList;
+
+import java.util.List;
+import java.util.ListIterator;
+
+public class nbtHelper {
+
+    public static NbtList createNbtListFromFloatArray(Float[] arr) {
+        List<Float> list = List.of(arr);
+        return createNbtListFromFloatArray(list);
+    }
+
+    public static NbtList createNbtListFromFloatArray(List<Float> arr) {
+        NbtList nbt = new NbtList();
+        ListIterator<Float> it = arr.listIterator();
+        while (it.hasNext()) {
+            nbt.add(it.nextIndex(), NbtFloat.of(it.next()));
+        }
+        return nbt;
+    }
+}
