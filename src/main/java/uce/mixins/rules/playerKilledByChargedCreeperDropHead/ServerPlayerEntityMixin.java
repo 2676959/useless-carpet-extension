41c109a76ed0255907dfd4d0190462d069068299
2676959 <p2676959@gmail.com>
2024-07-28 00:42:54 +0800

    fix the logic error

--- src/main/java/uce/mixins/rules/playerKilledByChargedCreeperDropHead/ServerPlayerEntityMixin.java
+++ src/main/java/uce/mixins/rules/playerKilledByChargedCreeperDropHead/ServerPlayerEntityMixin.java
@@ -1,7 +1,6 @@
 package uce.mixins.rules.playerKilledByChargedCreeperDropHead;
 
 import com.mojang.authlib.GameProfile;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -12,6 +11,7 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -20,21 +20,19 @@ import static uce.UselessCarpetExtensionSettings.playerKilledByChargedCreeperDro
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin extends PlayerEntity {
 
+    @Shadow public abstract void enterCombat();
+
     public ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
         super(world, pos, yaw, gameProfile);
     }
 
-    @Inject(at = @At("TAIL"), method = "onDeath")
+    @Inject(method = "onDeath", at = @At("TAIL"))
     private void dropPlayerHead(DamageSource damageSource, CallbackInfo info) {
-        if (!playerKilledByChargedCreeperDropHead) {
-            return;
-        }
-        CreeperEntity creeperEntity;
-        Entity entity = damageSource.getAttacker();
         // Different from vanilla mechanics, all players killed by the same creeper will drop their heads.
-        if (entity instanceof CreeperEntity) {
-            creeperEntity = (CreeperEntity) entity;
-            creeperEntity.onHeadDropped();
+        if (playerKilledByChargedCreeperDropHead
+                && damageSource.getAttacker() instanceof CreeperEntity entity
+                && entity.shouldRenderOverlay()) {
+            entity.onHeadDropped();
             NbtCompound playerHeadNbt = new NbtCompound();
             playerHeadNbt.putString("SkullOwner", super.getName().getString());
             ItemStack playerHeadItem = new ItemStack(Items.PLAYER_HEAD);
